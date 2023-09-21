6402856d118865350f3987be45f2a43e5164900f
Elvira Sidorenko <elvira.sidorenko@developersinstitute.co.nz>
2023-09-21 14:01:25 +1200

    Create flashcards quiz

--- flashcards-quiz-project/src/App.jsx
+++ flashcards-quiz-project/src/App.jsx
@@ -1,30 +1,92 @@
-import { useState } from "react";
-import FlashcardList from "./FlashcardList";
+import { useState, useEffect, useRef } from "react";
+import FlashcardList from "./components/FlashcardList";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS);
-  return <FlashcardList flashcards={flashcards} />;
-}
+  const [flashcards, setFlashcards] = useState([]);
+  const [categories, setCategories] = useState([]);
 
-const SAMPLE_FLASHCARDS = [
-  {
-    id: 1,
-    question: "What is 2 + 2?",
-    answer: "4",
-    options: ["1", "2", "4", "22"],
-  },
-  {
-    id: 2,
-    question: "What is the capital of Australia?",
-    answer: "Wellington",
-    options: ["Wellington", "Canberra", "Auckland", "Sydney"],
-  },
-  {
-    id: 3,
-    question: "What is useState in React.js?",
-    answer: "hook",
-    options: ["hook", "function", "parameter", "attribute"],
-  },
-];
+  const categoryEl = useRef();
+  const amountEl = useRef();
+
+  useEffect(() => {
+    axios.get("https://opentdb.com/api_category.php").then((res) => {
+      setCategories(res.data.trivia_categories);
+    });
+  }, []);
+
+  useEffect(() => {}, []);
+
+  function decodeString(str) {
+    const textArea = document.createElement("textarea");
+    textArea.innerHTML = str;
+    return textArea.value;
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    axios
+      .get("https://opentdb.com/api.php", {
+        params: {
+          amount: amountEl.current.value,
+          category: categoryEl.current.value,
+        },
+      })
+      .then((res) => {
+        setFlashcards(
+          res.data.results.map((questionItem, index) => {
+            const answer = decodeString(questionItem.correct_answer);
+            const options = [
+              ...questionItem.incorrect_answers.map((a) => decodeString(a)),
+              answer,
+            ];
+            return {
+              id: `${index}-${Date.now()}`,
+              question: decodeString(questionItem.question),
+              answer: answer,
+              options: options.sort(() => Math.random() - 0.5),
+            };
+          })
+        );
+      });
+  }
+
+  return (
+    <>
+      <form className="header" onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="category">Category</label>
+          <select id="category" ref={categoryEl}>
+            {categories.map((category) => {
+              return (
+                <option value={category.id} key={category.id}>
+                  {category.name}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+        <div className="form-group">
+          <label htmlFor="amount">Number of Questions</label>
+          <input
+            type="number"
+            id="amount"
+            min="1"
+            step="1"
+            defaultValue={10}
+            ref={amountEl}
+          />
+        </div>
+        <div className="form-group">
+          <button className="btn">Generate</button>
+        </div>
+      </form>
+      <div className="container">
+        <FlashcardList flashcards={flashcards} />
+      </div>
+    </>
+  );
+}
 
 export default App;
--- flashcards-quiz-project/src/components/FlashcardList.jsx
+++ flashcards-quiz-project/src/components/FlashcardList.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import Flashcard from "./Flashcard";
 
 export default function FlashcardList({ flashcards }) {
